c63b684880959eacac439cafd559789531af4d31
Fayzullo <fayzullarakhmatullayev@gmail.com>
2026-01-19 17:29:03 +0500

    init

--- src/components/refine-ui/layout/layout.tsx
+++ src/components/refine-ui/layout/layout.tsx
@@ -8,35 +8,35 @@ import type { PropsWithChildren } from "react";
 import { Sidebar } from "./sidebar";
 
 export function Layout({ children }: PropsWithChildren) {
-  return (
-    <ThemeProvider>
-      <SidebarProvider>
-        <Sidebar />
-        <SidebarInset>
-          <Header />
-          <main
-            className={cn(
-              "@container/main",
-              "container",
-              "mx-auto",
-              "relative",
-              "w-full",
-              "flex",
-              "flex-col",
-              "flex-1",
-              "px-2",
-              "pt-4",
-              "md:p-4",
-              "lg:px-6",
-              "lg:pt-6"
-            )}
-          >
-            {children}
-          </main>
-        </SidebarInset>
-      </SidebarProvider>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider>
+			<SidebarProvider>
+				<Sidebar />
+				<SidebarInset>
+					<Header />
+					<main
+						className={cn(
+							"@container/main",
+							"container",
+							"mx-auto",
+							"relative",
+							"w-full",
+							"flex",
+							"flex-col",
+							"flex-1",
+							"px-2",
+							"pt-4",
+							"md:p-4",
+							"lg:px-6",
+							"lg:pt-6"
+						)}
+					>
+						{children}
+					</main>
+				</SidebarInset>
+			</SidebarProvider>
+		</ThemeProvider>
+	);
 }
 
 Layout.displayName = "Layout";
--- src/components/refine-ui/notification/toaster.tsx
+++ src/components/refine-ui/notification/toaster.tsx
@@ -4,20 +4,20 @@ import { useTheme } from "@/components/refine-ui/theme/theme-provider";
 import { Toaster as Sonner, type ToasterProps } from "sonner";
 
 export function Toaster({ ...props }: ToasterProps) {
-  const { theme = "system" } = useTheme();
+	const { theme = "system" } = useTheme();
 
-  return (
-    <Sonner
-      theme={theme as ToasterProps["theme"]}
-      className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
-      {...props}
-    />
-  );
+	return (
+		<Sonner
+			theme={theme as ToasterProps["theme"]}
+			className="toaster group"
+			style={
+				{
+					"--normal-bg": "var(--popover)",
+					"--normal-text": "var(--popover-foreground)",
+					"--normal-border": "var(--border)"
+				} as React.CSSProperties
+			}
+			{...props}
+		/>
+	);
 }
