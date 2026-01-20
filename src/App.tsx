84cd634a5b4fb1e38733bef47120dfab84551b5f
Fayzullo <fayzullarakhmatullayev@gmail.com>
2026-01-20 09:21:57 +0500

    refactor: enhance styling and structure for subjects management, add role and department constants, and implement validation schemas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
 import routerProvider, { DocumentTitleHandler, UnsavedChangesNotifier } from "@refinedev/react-router";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router";
-import "./App.css";
-import { Toaster } from "./components/refine-ui/notification/toaster";
-import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
-import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
-import { dataProvider } from "./providers/data";
-import Dashboard from "@/pages/dashboard.tsx";
+
 import { BookOpen, Home } from "lucide-react";
-import { Layout } from "@/components/refine-ui/layout/layout.tsx";
-import SubjectsList from "@/pages/subjects/list.tsx";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+
+import { dataProvider } from "./providers/data";
+import "./App.css";
+
+import Dashboard from "@/pages/dashboard.tsx";
+import { Toaster } from "@/components/refine-ui/notification/toaster";
 import SubjectsCreate from "@/pages/subjects/create.tsx";
+
+import { Layout } from "@/components/refine-ui/layout/layout.tsx";
+import { ThemeProvider } from "@/components/refine-ui/theme/theme-provider";
+import { useNotificationProvider } from "@/components/refine-ui/notification/use-notification-provider";
+import SubjectsList from "@/pages/subjects/list.tsx";
 
 function App() {
 	return (
